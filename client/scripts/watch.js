a244184f8e9ab03c57c3a10e15c0fc8c840290e3
Alexander <zver25@gmail.com>
2020-01-27 10:29:44 +1000

    [Client] Repair watch.js. Put file to build/ dir.

--- client/scripts/watch.js
+++ client/scripts/watch.js
@@ -1,9 +1,14 @@
 process.env.NODE_ENV = 'development';
 
 const fs = require('fs-extra');
+const path = require('path');
 const paths = require('react-scripts/config/paths');
 const webpack = require('webpack');
 const config = require('react-scripts/config/webpack.config.js')('development');
+
+config.entry = config.entry.filter(fileName => !fileName.match(/webpackHotDevClient/));
+config.plugins = config.plugins.filter(plugin => !(plugin instanceof webpack.HotModuleReplacementPlugin));
+config.output.path = path.resolve(__dirname, '..', 'build');
 
 webpack(config).watch({}, (err, stats) => {
 	if (err) {
