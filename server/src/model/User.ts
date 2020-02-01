a9d789e7078a2eb8063a88a57820289aeea19d53
Александр Свиридовский <zver25@gmail.com>
2020-02-01 11:31:03 +1000

    Update User.ts
    [Server] model/User

--- server/src/model/User.ts
+++ server/src/model/User.ts
@@ -1,6 +1,9 @@
 class User {
 
-	constructor(private _name: string) {}
+	private _password: sring;
+	private _token: string;
+
+	constructor(private _id: number, private _name: string) {}
 
 	public toJson(): object {
 		return {
@@ -11,6 +14,23 @@ class User {
 	public getName(): string {
 		return this._name;
 	}
+
+	public setPassword(password: string) {
+		this._password = password;
+	}
+
+	public getPassword(): string {
+		return this._password;
+	}
+
+	public setToken(token: string) {
+		this._token = token;
+	}
+
+	public getToken(): string {
+		return this._token;
+	}
+
 }
 
 export default User;
